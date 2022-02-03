9e78206a712923d5f34f85588e3ad4930d18532f
DESKTOP-P7TDC68\Sujin <resqpa79@gmail.com>
2022-02-03 23:09:10 +0900

    함수 컴포넌트

---
+++ ClientApp/src/components/Books/SeeSharpGuide.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SeeSharpGuide() {
+    return (
+      <div>
+        <h1>강좌 리스트</h1>
+        <ul>
+          <li>리액트 소개</li>
+          <li>리액트 개발환경구축</li>
+          <li>리액트 HelloWorld</li>
+        </ul>
+      </div>
+    )
+}
+
+export default SeeSharpGuide;
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -6,14 +6,12 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+import SeeSharpGuide from './components/Books/SeeSharpGuide';
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
+  <SeeSharpGuide />
+  ,
+  document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
